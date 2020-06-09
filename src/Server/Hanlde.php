094c266bde9862a877794d85f4cbd087b9778cdd
clouds-flight <1730848300@qq.com>
2020-06-09 12:28:50 +0800

    Hanlde类添加过滤属性，防止非法调用函数

--- src/Server/Hanlde.php
+++ src/Server/Hanlde.php
@@ -3,7 +3,7 @@
 /*
  * @Author: 吴云祥
  * @Date: 2020-06-08 21:04:30
- * @LastEditTime: 2020-06-09 09:14:29
+ * @LastEditTime: 2020-06-09 12:27:36
  * @FilePath: /swoole-rpc/src/Server/Hanlde.php
  */
 
@@ -13,15 +13,20 @@ namespace Swoole\Rpc\Server;
 abstract class Hanlde
 {
 
+    private $filter=[];
+
     public function deal($name, $arguments)
     {
+        $name=trim($name);
         $return = [];
         try {
-            if (!method_exists($this, $name)) {
+            
+            if (!method_exists($this, $name)||$name=='deal'||in_array($name,$this->filter)) {
                 $return['status'] = 0;
                 $return['msg'] = '执行失败,'.$name.'方法不存在';
                 $return['data'] =null;
             } else {
+                
                 $data = $this->{$name}(...$arguments);
                 $return['status'] = 1;
                 $return['msg'] = '执行成功';
