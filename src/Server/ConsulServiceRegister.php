b2350547eee2265c3f1ab592a414953bd9eccfc3
clouds-flight <1730848300@qq.com>
2020-06-10 08:25:48 +0800

    添加consul注册类其它参数配置

--- src/Server/ConsulServiceRegister.php
+++ src/Server/ConsulServiceRegister.php
@@ -3,8 +3,8 @@
 /*
  * @Author: 吴云祥
  * @Date: 2020-06-08 22:16:37
- * @LastEditTime: 2020-06-09 10:20:48
- * @FilePath: /swoole-rpc/src/Server/ConsulServiceRegister.php
+ * @LastEditTime: 2020-06-10 08:23:49
+ * @FilePath: /pf-connection-server/vendor/clouds-flight/swoole-rpc/src/Server/ConsulServiceRegister.php
  */
 
 namespace Swoole\Rpc\Server;
@@ -35,12 +35,60 @@ class ConsulServiceRegister implements ServiceRegisterInterface
         $serviceHelper->address = $service->host;
         $serviceHelper->port = $service->port;
 
+        if (isset($service->extends['tags'])) {
+            $serviceHelper->enableTagOverride = $service->extends['tags'];
+        }
+
+        if (isset($service->extends['meta'])) {
+            $serviceHelper->enableTagOverride = $service->extends['meta'];
+        }
+
+        if (isset($service->extends['taggedAddresses'])) {
+            $serviceHelper->enableTagOverride = $service->extends['taggedAddresses'];
+        }
+
         if (isset($service->extends['enableTagOverride'])) {
             $serviceHelper->enableTagOverride = $service->extends['enableTagOverride'];
         }
 
         if (isset($service->extends['check'])) {
             $serviceHelper->check = $service->extends['check'];
+        }
+
+        if (isset($service->extends['checks'])) {
+            $serviceHelper->check = $service->extends['checks'];
+        }
+
+        if (isset($service->extends['kind'])) {
+            $serviceHelper->check = $service->extends['kind'];
+        }
+
+        if (isset($service->extends['proxyDestination'])) {
+            $serviceHelper->check = $service->extends['proxyDestination'];
+        }
+
+        if (isset($service->extends['proxy'])) {
+            $serviceHelper->check = $service->extends['proxy'];
+        }
+
+        if (isset($service->extends['expose'])) {
+            $serviceHelper->check = $service->extends['expose'];
+        }
+
+        if (isset($service->extends['connect'])) {
+            $serviceHelper->check = $service->extends['connect'];
+        }
+
+        if (isset($service->extends['weights'])) {
+            $serviceHelper->check = $service->extends['weights'];
+        }
+
+        if (isset($service->extends['token'])) {
+            $serviceHelper->check = $service->extends['token'];
+        }
+
+        if (isset($service->extends['namespace'])) {
+            $serviceHelper->check = $service->extends['namespace'];
         }
 
         return $serviceHelper->register();
